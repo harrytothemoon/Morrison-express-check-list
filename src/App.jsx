fbdb777beb81cde9067723d6dd970b2b8e977de2
Harry <harry811016@gmail.com>
2022-03-03 14:17:07 +0800

    style: layout the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">123</div>;
+  const renderHeader = () => {
+    return (
+      <tr>
+        <th class="checkboxCol">
+          <input type="checkbox" />
+        </th>
+        <th></th>
+        <th>狀態</th>
+      </tr>
+    );
+  };
+  const renderBody = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td></td>
+          <td>00-未派車</td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td></td>
+          <td>00-未派車</td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td></td>
+          <td>00-未派車</td>
+        </tr>
+      </tbody>
+    );
+  };
+  return (
+    <div className="App">
+      <h1 className="title">Checkboxes List</h1>
+      <table className="table">
+        <thead>{renderHeader()}</thead>
+        {renderBody()}
+      </table>
+    </div>
+  );
 }
 
 export default App;
